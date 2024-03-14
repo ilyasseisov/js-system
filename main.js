70c52c41ea9bbe1fa0e859dd321e1be0a9307832
Ilyas Seisov <ilyasseisov@gmail.com>
2024-03-14 16:52:11 +0500

    add matrix

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ import './css/style.css';
 import Cursor from './js/Cursor';
 import SmoothScroll from './js/Lenis';
 import AnimateOnScroll from './js/AOS';
+import Matrix from './js/Matrix';
 
 // function calls
 Cursor();
 SmoothScroll();
 AnimateOnScroll();
+Matrix();
